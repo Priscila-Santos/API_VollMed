67b088f7f9d11b5956356a8452f2400b49452917
Priscila Santos <priscila15dsantos@gmail.com>
2025-07-03 15:51:58 -0300

    add listar medicos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,11 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medicos.DadosCadastroMedico;
+import med.voll.api.medicos.DadosListagemMedico;
 import med.voll.api.medicos.Medico;
 import med.voll.api.medicos.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,6 +22,11 @@ public class MedicoController {
     @Transactional
     public void cadastrarMedico(@RequestBody @Valid DadosCadastroMedico dados) {
         medicoRepository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listarMedicos() {
+        return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 
 }
